9c53b34533e4245b51e538365a1fab8d1494b52a
matiudev <garcia90mati@gmail.com>
2024-05-17 16:20:05 -0400

    React Responsive Design

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import { ExpenseChart } from "./components/ExpenseChart";
 function App() {
   return (
     <GlobalProvider>
-      <div className="bg-neutral-950 text-white h-screen flex justify-center items-center">
-        <div className="w-2/5 flex justify-center items-center">
-          <div className="bg-neutral-800 p-10 rounded-md w-full">
+      <div className="bg-neutral-950 text-white min-h-screen flex justify-center items-center p-4">
+        <div className="w-full max-w-4xl flex flex-col items-center md:w-2/3 lg:w-2/5">
+          <div className="bg-neutral-800 p-6 md:p-10 rounded-md w-full">
             <Header />
             <div className="flex flex-col md:flex-row justify-between gap-4">
               <div className="flex-1">
